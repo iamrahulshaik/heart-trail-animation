022b9d88a7ec84bc547dd384d38cd44d38a9704d
Rahul <rahulshaik2808@gmail.com>
2025-05-13 13:07:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 const bodyEL = document.querySelector("body");
 
-bodyEL.addEventListener("mousemove", (Event) => {
+bodyEL.addEventListener("mousemove", (event) => {
   const xpos = event.offsetX;
   const ypos = event.offsetY;
+
   const spanEL = document.createElement("span");
   spanEL.style.left = xpos + "px";
   spanEL.style.top = ypos + "px";
-  const size = Math.random() * 100;
+
+  const size = Math.random() * 90;
   spanEL.style.width = size + "px";
   spanEL.style.height = size + "px";
+
   bodyEL.appendChild(spanEL);
+
   setTimeout(() => {
     spanEL.remove();
-  }, 3000);
+  }, 5000);
 });
